3b2e5a403283251dbe33f1b1998cf2f48a2770dc
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-01-21 18:24:37 +0200

    will start doing things on action events

--- src/main/java/swingy/controller/GUIController.java
+++ src/main/java/swingy/controller/GUIController.java
@@ -1,13 +1,39 @@
 package swingy.controller;
 
 import swingy.view.Menu;
+import swingy.model.characters.Hero;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+// If gui is in use, this class is used to communicate with models/other controllers
 public class GUIController {
-    private Menu menu;
-    public GUIController(Menu frame) {
-            menu = frame;
+    private static Menu menu;
+    private static File saveFile = new File("./save.txt");
+    private static PrintWriter printWriter;
+
+    static {
+        try {
+            printWriter = new PrintWriter(saveFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
-    String command = menu.getCommand();
+    public GUIController(Menu frame) {
+        System.out.println("From GUIController: Creating new GUIController");
+        menu = frame;
+    }
+
+    public static void interpretMenuCommand(String command) {
+        if (command.equals("newGame")) {
+            System.out.println("GUIController: Interpreted command");
+        }
+    }
+
+//    public Hero newHero(String characterName, String characterClass) {
+//        return new Hero(characterName, characterClass);
+//    }
 
 }
