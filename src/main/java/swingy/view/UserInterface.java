ef910e01651835095f3c4c781e554fbc810f75f2
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-01-21 10:00:02 +0200

    instantiates gui view, sets initial output. methods return type of interface and instantiated frame if gui

--- src/main/java/swingy/view/UserInterface.java
+++ src/main/java/swingy/view/UserInterface.java
@@ -2,13 +2,36 @@ package swingy.view;
 
 import javax.swing.*;
 
-public class UserInterface {
+public class UserInterface  {
+    private Menu frame;
+    public int interfaceType;
+//    private GameController gameController = new GameController();
+    public UserInterface(final String type) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                if (type.equalsIgnoreCase("gui")) {
+                    interfaceType = 1;
+                    frame = new Menu("Swingy: Origin of the Revengening Infinite The Movie The Game");
+                    frame.displayOutput("Welcome to swingy!\nWould you like to:\n\t1 - Start a new game\n\t2 - Load an existing game");
+                } else if (type.equalsIgnoreCase("console")) {
+                    interfaceType = 0;
+                    System.out.println("Welcome to Swingy: Origin of the Revengening Infinite The Movie The Game");
+                }
+            }
+        });
+    }
 
-    public UserInterface() {
-        JFrame frame = new JFrame("Swingy");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600, 500);
-        frame.setResizable(true);
-        frame.setVisible(true);
+    public int getInterfaceType() {
+        return this.interfaceType;
+    }
+
+//    public void getInterface(int type) {
+//        if (type == 1) {
+//            this.getFrame();
+//        }
+//    }
+
+    public Menu getFrame() {
+        return frame;
     }
 }
