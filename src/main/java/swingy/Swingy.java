7682b0288284da6dface5d2fc23d48ebfbeb354c
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-01-21 09:49:08 +0200

    instantiates userinterface and gamecontroller for gui or console

--- src/main/java/swingy/Swingy.java
+++ src/main/java/swingy/Swingy.java
@@ -1,15 +1,26 @@
 package swingy;
 
+import swingy.controller.GameController;
 import swingy.view.UserInterface;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import javax.validation.*;
 
 public class Swingy {
 
     private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    private static UserInterface userInterface = new UserInterface();
-    public static void main (String[] args) {
+    private static UserInterface userInterface;
+    private static GameController controller;
 
+    public static void main (String[] args) {
+        if (args.length > 0) {
+            userInterface = new UserInterface(args[0]);
+            controller = new GameController(userInterface);
+        } else {
+            System.out.println("Your arguments are invalid");
+            System.exit(1);
+        }
     }
 
 }
