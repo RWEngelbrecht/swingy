f6de71378fc6fb014d921745d16ad1545a5ac892
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-01-21 18:22:19 +0200

    became controller for general methods interacting with models, having nothing to do with the gui

--- src/main/java/swingy/controller/GameController.java
+++ src/main/java/swingy/controller/GameController.java
@@ -1,21 +1,48 @@
 package swingy.controller;
 
+import swingy.model.HeroFactory;
+import swingy.model.characters.Hero;
 import swingy.view.Menu;
 import swingy.view.UserInterface;
+import swingy.model.Game;
 
+// Either holds instance of GUIController or acts as controller for all console-related actions.
+// Also interacts with Game
 public class GameController {
+    protected static Game game;
     private GUIController guiController;
+    private int controllerType;
+    private static HeroFactory heroFactory = new HeroFactory();
     public GameController(UserInterface userInterface) {
-        if (userInterface.getInterfaceType() == 1) {
+        controllerType = userInterface.getInterfaceType();
+        if (controllerType == 1) {
+            System.out.println("from gameController: creating new GUIController");
             newGUIController(userInterface.getFrame());
         }
+        game = new Game(this);
     }
 
     private void newGUIController(Menu frame) {
         guiController = new GUIController(frame);
     }
 
-    public void startGame() {
-//        guiController.
+    public Game getGame() {
+        return this.game;
     }
+
+    public void newHero(String characterName, String characterClass) {
+        game.addHero(heroFactory.newHero(characterName, characterClass));
+    }
+
+    public void generateMap() {
+        game.makeMap();
+//        game.printMap();
+    }
+
+    public void interpretMenuCommand(String command) {
+        if (controllerType == 1) {
+            guiController.interpretMenuCommand(command);
+        }
+    }
+
 }
