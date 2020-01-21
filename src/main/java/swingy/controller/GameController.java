13c05e37a1b29e8084e0ae48d8f2f6f92f3fc023
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-01-21 09:53:36 +0200

    creates,stores controller based on interface type. will hold methods to interact with game

--- src/main/java/swingy/controller/GameController.java
+++ src/main/java/swingy/controller/GameController.java
@@ -1,5 +1,23 @@
 package swingy.controller;
 
-public class GameController {
+import swingy.view.MainMenu;
+import swingy.view.UserInterface;
 
+import javax.swing.JFrame;
+
+public class GameController {
+    private GUIController guiController;
+    public GameController(UserInterface userInterface) {
+        if (userInterface.getInterfaceType() == 1) {
+            newGUIController(userInterface.getFrame());
+        }
+    }
+
+    private void newGUIController(MainMenu frame) {
+        guiController = new GUIController(frame);
+    }
+
+    public void startGame() {
+//        guiController.
+    }
 }
