cd2fc22fafc78a91601b58d2ddb464d22869de21
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-01-22 17:47:01 +0200

    added method to get instance info as string

--- src/main/java/swingy/model/characters/Hero.java
+++ src/main/java/swingy/model/characters/Hero.java
@@ -7,6 +7,8 @@ public abstract class Hero {
     protected static int hp;
     protected static int xp;
     protected static int lvl;
+    protected static int atk;
+    protected static int def;
 
     public Hero(String name, String charClass) {
         this.name = name;
@@ -17,4 +19,27 @@ public abstract class Hero {
     public String getHeroClass() { return this.charClass; }
     public abstract int getHp();
     public abstract int getLevel();
+    public abstract int getXp();
+    public abstract int getAtk();
+    public abstract int getDef();
+
+    public String getAllInfo() {
+        String lvl = Integer.toString(this.getLevel());
+        String xp = Integer.toString(this.getXp());
+        String hp = Integer.toString(this.getHp());
+        String atk = Integer.toString(this.getAtk());
+        String def = Integer.toString(this.getDef());
+
+
+        String allInfo = this.getName();
+        allInfo = allInfo.concat(","+getHeroClass());
+        allInfo = allInfo.concat(","+lvl);
+        allInfo = allInfo.concat(","+xp);
+        allInfo = allInfo.concat(","+hp);
+        allInfo = allInfo.concat(","+atk);
+        allInfo = allInfo.concat(","+def);
+
+        return allInfo;
+    }
 }
+//<name>,<class>,<xp>,<atk>,<def>,<weapon>,<armor>
