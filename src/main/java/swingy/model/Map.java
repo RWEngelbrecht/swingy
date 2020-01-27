6d590afe36dd63eaffe7970e47ed3192bb884e65
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-01-27 18:15:39 +0200

    can get int representation of hero position on map + return int rep of what else is in position hero moved to

--- src/main/java/swingy/model/Map.java
+++ src/main/java/swingy/model/Map.java
@@ -26,6 +26,20 @@ public class Map {
         }
     }
 
+    private int[] getHeroPosition(int size) {
+
+        int[] pos = new int[2];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (map[i][j] == 1) {
+                    pos[0] = i;
+                    pos[1] = j;
+                }
+            }
+        }
+        return pos;
+    }
+
     public int getMapSize() {
         int count = 0;
         while (count < map.length) {
@@ -38,12 +52,27 @@ public class Map {
         return map;
     }
 
-    public void moveHero(String direction) {
-        int spaces = 0;
-        if (direction.equalsIgnoreCase("north") || direction.equalsIgnoreCase("east"))
-            spaces = 1;
-        else if (direction.equalsIgnoreCase("south") || direction.equalsIgnoreCase("west"))
-            spaces = -1;
-        System.out.println("Map: spaces = "+spaces);
+    public int moveHero(String direction) {
+        int size = getMapSize();
+        int locationStatus = -1;
+        int[] pos = getHeroPosition(size);
+        System.out.println("Map: pos of hero: y: "+pos[0]+" x: "+pos[1]);
+        if (direction.equalsIgnoreCase("north")) {
+            if (pos[0] + 1 < size)
+                locationStatus = map[pos[0] + 1][pos[1]];
+        }
+        else if (direction.equalsIgnoreCase("east")) {
+            if (pos[1] + 1 < size)
+                locationStatus = map[pos[0]][pos[1] + 1];
+        }
+        else if (direction.equalsIgnoreCase("south")) {
+            if (pos[0] - 1 > 0)
+                locationStatus = map[pos[0] - 1][pos[1]];
+        }
+        else if (direction.equalsIgnoreCase("west")) {
+            if (pos[1] - 1 > 0)
+                locationStatus = map[pos[0]][pos[1] - 1];
+        }
+        return locationStatus;
     }
 }
