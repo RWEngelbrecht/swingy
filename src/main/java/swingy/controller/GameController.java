e47fee1abbafa4ae2075979cf54512b8f6846c15
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-01-27 09:39:12 +0200

    handles both gui+console. calls datahandler to save + load games

--- src/main/java/swingy/controller/GameController.java
+++ src/main/java/swingy/controller/GameController.java
@@ -1,7 +1,9 @@
 package swingy.controller;
 
+import swingy.model.DataHandler;
 import swingy.model.HeroFactory;
 import swingy.model.characters.Hero;
+import swingy.view.ConsoleMenu;
 import swingy.view.GUIMenu;
 import swingy.view.UserInterface;
 import swingy.model.Game;
@@ -9,37 +11,51 @@ import swingy.model.Game;
 import javax.validation.constraints.NotBlank;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 // Either holds instance of GUIController or acts as controller for all console-related actions.
 // Also interacts with Game
 public class GameController {
     protected static Game game;
-    private GUIController guiController;
+//    private GUIController guiController;
     private int controllerType;
+
+    private static DataHandler dataHandler = new DataHandler();
     private static HeroFactory heroFactory = new HeroFactory();
     private static File saveFile;
 
+
     public GameController(UserInterface userInterface) {
         controllerType = userInterface.getInterfaceType();
-        if (controllerType == 1) {
-            System.out.println("from gameController: creating new GUIController");
-            newGUIController(userInterface.getFrame());
-        }
+//        if (controllerType == 1) {
+//            System.out.println("from gameController: creating new GUIController");
+//            newGUIController(userInterface.getFrame());
+//        }
         game = new Game(this);
     }
 
-    private void newGUIController(GUIMenu frame) {
-        guiController = new GUIController(frame);
-    }
+//    private void newGUIController(GUIMenu frame) {
+//        guiController = new GUIController(frame);
+//    }
 
     public Game getGame() {
         return this.game;
     }
 
+    public void interpretConsole() {
+        Scanner scanner = new Scanner(System.in);
+        ConsoleMenu consoleMenu = new ConsoleMenu();
+        consoleMenu.getMenuControls("main");
+        String command;
+        while ((command = scanner.nextLine()) != null) {
+
+        }
+    }
+
     public void newHero(@NotBlank String characterName, @NotBlank String characterClass) {
         String charClass;
         if (controllerType == 1) {
-            charClass = new String(guiController.getClassFromGui(characterClass));
+            charClass = new String(dataHandler.getClassFromGui(characterClass));
         } else {
             if (characterClass.equals("1"))
                 charClass = new String("Warrior");
@@ -55,48 +71,40 @@ public class GameController {
     }
 
     public void loadHero(String heroInfo) {
-        //TODO: CREATE LOADHERO
         Hero hero = heroFactory.loadHero(heroInfo);
         game.addHero(hero);
     }
 
     public void generateMap() {
         game.makeMap();
-        System.out.println("Our hero "+game.getHero().getName()+" the "+game.getHero().getHeroClass()+" sets off on their adventure!");
+        System.out.println("Our hero "+game.getHero().getName()+" the "+game.getHero().getHeroClass()+" of level "+game.getHero().getLevel()+" sets off on their adventure!");
 //        game.printMap();
     }
 
-    public void validateInput(String value, String context) {
-        if (controllerType == 1) {
-            guiController.validateInput(value, context);
-        }
-    }
+//    public void validateInput(String value, String context) {
+//        if (controllerType == 1) {
+//            guiController.validateInput(value, context);
+//        }
+//    }
 
     public void createSaveFile(/*Hero hero*/) {
-        if (controllerType == 1) {
-            saveFile = guiController.createSaveFile();
-        }
+        saveFile = dataHandler.createSaveFile();
     }
 
-    public void saveGame(Hero hero) {
-        if (controllerType == 1) {
+    public void saveGame() {
+//        if (controllerType == 1) {
             System.out.println("GameController: save game reached");
-            guiController.saveGame(saveFile, hero);
-        }
+            Hero hero = game.getHero();
+            dataHandler.saveGame(saveFile, hero);
+//        }
     }
 
     public ArrayList<String> getSavedGames() {
-        if (controllerType == 1) {
-            return guiController.getSavedGames(saveFile);
-        }
-        return null;
+        return dataHandler.getSavedGames(saveFile);
     }
 
     public ArrayList<String> getSavesForGui() {
-        if (controllerType == 1) {
-            return guiController.getSavesForGui(saveFile);
-        }
-        return null;
+        return dataHandler.getSavesForGui(saveFile);
     }
 
     public void loadGame(String gameNumberStr) {
