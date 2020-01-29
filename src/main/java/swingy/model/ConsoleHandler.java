e625830eaf9261adf51eb28fa1d37923b40565a8
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-01-29 15:32:19 +0200

    methods that tell gameController what to do with input from consoleMenu view

--- src/main/java/swingy/model/ConsoleHandler.java
+++ src/main/java/swingy/model/ConsoleHandler.java
@@ -2,6 +2,7 @@ package swingy.model;
 
 import swingy.controller.GameController;
 
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 
 public class ConsoleHandler {
@@ -11,9 +12,37 @@ public class ConsoleHandler {
     }
 
     public void startCreateHero() {
-
+        gameController.consoleCreateHeroControls();
     }
 
-    public void startLoadHero(ArrayList<String> saves) {
+    public boolean checkHeroName(@NotNull String heroName) {
+        return heroName.length() > 0;
+    }
+
+    public boolean checkHeroClass(@NotNull String heroClass) {
+        return heroClass.equals("1") || heroClass.equals("2") || heroClass.equals("3");
+    }
+
+    public void startGame() {
+        gameController.generateMap();
+        gameController.consoleStartGame();
+    }
+
+    public void continueGame(int positionState) {
+        if (positionState == 0) {
+            gameController.reactEmptySpace();
+        } else if (positionState == 2) {
+            gameController.reactEnemySpace();
+        } else if (positionState == 3) {
+            gameController.reactArtifactSpace();
+        }
+    }
+
+    public void startLoadHero() {
+        ArrayList<String> saves = gameController.getPrintableSaves();
+        if (saves != null)
+            gameController.consoleLoadHeroControls(saves);
+        else
+            gameController.consoleGenerateMainMenu();
     }
 }
