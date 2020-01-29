c286dd0889f0dc806cd80dd7c1714b7b86dcb465
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-01-29 12:18:41 +0200

    updated run.sh to take console or gui or console input

--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
-mvn clean package
-java -cp target/swingy-1.0-SNAPSHOT.jar swingy.Swingy gui
+if [ $# -gt 0 ] && [ $# -lt 2 ]
+then 
+	echo "starting swingy in $1"
+	mvn clean package
+	java -cp target/swingy-1.0-SNAPSHOT.jar swingy.Swingy $1
+else
+	echo "run script with gui or console arguments"
+fi
+	
