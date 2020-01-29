8b4724b6883e71a7cfa95b9d93d83cc4038372fb
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-01-29 18:07:24 +0200

    can equip artifacts to hero, generate random enemy/artifact

--- src/main/java/swingy/model/Game.java
+++ src/main/java/swingy/model/Game.java
@@ -3,15 +3,40 @@ package swingy.model;
 import swingy.controller.GameController;
 import swingy.model.characters.Hero;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
 // Keeps all information about current game i.e. the map, the hero
 public class Game {
     private static GameController gameController;
     private static Hero hero;
     private static Map map;
+    private static ArrayList<ArrayList<String>> artifacts;
+    private static ArrayList<String> enemies;
+    private static String currArtifact;
+    private static String currEnemy;
     private static boolean isActive;
     public Game(GameController gameController) {
         isActive = true;
         this.gameController = gameController;
+
+        enemies = new ArrayList<String>(Arrays.asList(
+                "Two-Toed Sloth", "Duck With a Knife", "Government Drone",
+                "Your Demons", "Bag of Snapping Turtles"));
+
+        artifacts = new ArrayList<ArrayList<String>>();
+
+        ArrayList<String> weapons = new ArrayList<String>(Arrays.asList(
+                "Spoon", "Brick", "Sharp Wit", "Lightsaber", "Pocket Knife"
+        ));
+        ArrayList<String> armors = new ArrayList<String>(Arrays.asList(
+                "Iron Kettle Helm", "Self Respect", "Chainmail Slipper",
+                "Steel Speedo", "Legendary Stone Mittens"
+        ));
+
+        artifacts.add(weapons);
+        artifacts.add(armors);
     }
 
     public void addHero(Hero hero) {
@@ -19,12 +44,21 @@ public class Game {
     }
 
     public void makeMap() {
-        map = new Map(hero.getLevel());
+        map = new Map(1/*hero.getLevel()*/);
     }
 
     public int moveHero(String direction) {
-        return map.moveHero(direction);
+        int locationStatus = map.moveHero(direction);
+        if (locationStatus == 2) {
+            currEnemy = generateEnemy();
+        } else if (locationStatus == 3) {
+            currArtifact = generateArtifact();
+        }
+        return locationStatus;
     }
+
+    public String getCurrArtifact() { return currArtifact; }
+    public String getCurrEnemy() { return currEnemy; }
 
     public void printMap() {
 //        System.out.println("Game: mapsize = "+map.getMapSize());
@@ -41,9 +75,32 @@ public class Game {
 
     public static boolean gameIsActive() { return isActive; }
 
+    public String generateEnemy() {
+        Random r = new Random();
+
+        return enemies.get(r.nextInt(enemies.size()));
+    }
+
+    private String generateArtifact() {
+        Random r = new Random();
+
+        return artifacts.get(r.nextInt(2)).get(r.nextInt(5));
+    }
+
     public void fight() {
+        // calculate result of battle
     }
 
     public void run() {
+        // do not update map position
+    }
+
+    public void equipArtifact() {
+        //TODO: Specific atk/def buffs/de-buffs depending on artifact;
+        if (artifacts.get(0).contains(currArtifact)) {
+            hero.equipWeapon(currArtifact, 1);
+        } else {
+            hero.donArmor(currArtifact, 1);
+        }
     }
 }
