2db3edd85ff70a3f7b4d5b6a8037fd8ba80eede1
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-01-29 12:19:59 +0200

    will act as model to consoleMenu view

---
+++ src/main/java/swingy/model/ConsoleHandler.java
@@ -0,0 +1,19 @@
+package swingy.model;
+
+import swingy.controller.GameController;
+
+import java.util.ArrayList;
+
+public class ConsoleHandler {
+    private static GameController gameController;
+    public ConsoleHandler(GameController gameController) {
+        this.gameController = gameController;
+    }
+
+    public void startCreateHero() {
+        
+    }
+
+    public void startLoadHero(ArrayList<String> saves) {
+    }
+}
