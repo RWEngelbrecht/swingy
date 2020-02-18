6faee6502d98fd69be7229d8ab2e3d1fce343d8d
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-02-18 12:10:26 +0200

    add support for multiple rolls of same die

--- src/main/java/swingy/model/Dice.java
+++ src/main/java/swingy/model/Dice.java
@@ -7,23 +7,34 @@ import java.util.Random;
 public class Dice {
     private static Random r = new Random();
 
-    public int roll(@NotNull String die) {
+    public int roll(@NotNull String dice) {
+        String amountAndDie[] = dice.split("d");
+        String die = amountAndDie[1];
+        int amount = Integer.parseInt(amountAndDie[0]);
+
         int roll = 1;
         // request random int from smaller range and add one, otherwise 1 is more likely to be generated than others
-        if (die.equals("d100"))
-            roll += r.nextInt(101 - 1);
-        else if (die.equals("d20"))
-            roll += r.nextInt(21 - 1);
-        else if (die.equals("d12"))
-            roll += r.nextInt(13 - 1);
-        else if (die.equals("d10"))
-            roll += r.nextInt(11 - 1);
-        else if (die.equals("d6"))
-            roll += r.nextInt(7 - 1);
-        else if (die.equals("d4"))
-            roll += r.nextInt(5 - 1);
-        else if (die.equals("d2"))
-            roll += r.nextInt(3 - 1);
+        if (die.equals("100"))
+            for (int i = 0; i < amount; i++)
+                roll += r.nextInt(101 - 1);
+        else if (die.equals("20"))
+            for (int i = 0; i < amount; i++)
+                roll += r.nextInt(21 - 1);
+        else if (die.equals("12"))
+            for (int i = 0; i < amount; i++)
+                roll += r.nextInt(13 - 1);
+        else if (die.equals("10"))
+            for (int i = 0; i < amount; i++)
+                roll += r.nextInt(11 - 1);
+        else if (die.equals("6"))
+            for (int i = 0; i < amount; i++)
+                roll += r.nextInt(7 - 1);
+        else if (die.equals("4"))
+            for (int i = 0; i < amount; i++)
+                roll += r.nextInt(5 - 1);
+        else if (die.equals("2"))
+            for (int i = 0; i < amount; i++)
+                roll += r.nextInt(3 - 1);
         return roll;
     }
 
