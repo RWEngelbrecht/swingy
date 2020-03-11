9c5bd7022cb4f8a30619725d7dad138c74514a96
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-03-11 11:57:58 +0200

    updated to compile and run as described in pdf

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ if [ $# -gt 0 ] && [ $# -lt 2 ]
 then 
 	echo "starting swingy in $1"
 	mvn clean package
-	java -cp target/swingy-1.0-SNAPSHOT.jar swingy.Swingy $1
+	java -jar target/swingy-1.0-SNAPSHOT.jar $1
 else
 	echo "run script with gui or console arguments"
 fi
