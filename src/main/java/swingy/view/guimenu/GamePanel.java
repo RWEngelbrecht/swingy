15956f2a094fee97f7b092c70b96706c5b37e5dc
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-03-16 09:51:46 +0200

    added death screen controls. updated looting controls

--- src/main/java/swingy/view/guimenu/GamePanel.java
+++ src/main/java/swingy/view/guimenu/GamePanel.java
@@ -11,26 +11,24 @@ import java.awt.event.ActionListener;
 //TODO: MORE Visual output for movement/fighting/loot
 public class GamePanel extends JPanel {
     private static GameController gameController;
-    private static JPanel freeRoamTools = new JPanel();
-//    private static JPanel enemyEncounterTools = new JPanel();
-//    private static JPanel lootingTools = new JPanel();
+    private static JPanel gamePanelControls = new JPanel();
 
-    private static JButton north = new JButton("North");
-    private static JButton east = new JButton("East");
-    private static JButton west = new JButton("West");
-    private static JButton south = new JButton("South");
+    private JButton north = new JButton("North");
+    private JButton east = new JButton("East");
+    private JButton west = new JButton("West");
+    private JButton south = new JButton("South");
 
-    private static JPanel systemTools = new JPanel();
-    private static JButton save = new JButton("Save");
-    private static JButton exit = new JButton("Exit");
+    private JPanel systemTools = new JPanel();
+    private JButton save = new JButton("Save");
+    private JButton exit = new JButton("Exit");
 
-    private static JButton fight = new JButton("Fight");
-    private static JButton run = new JButton("Run");
+    private JButton fight = new JButton("Fight");
+    private JButton run = new JButton("Run");
 
-    private static JButton equip = new JButton("Equip");
-    private static JButton ignore = new JButton("Ignore");
+    private JButton equip = new JButton("Equip");
+    private JButton ignore = new JButton("Ignore");
 
-    private static JTextArea gameOutput = new JTextArea(32, 20);
+    private JTextArea gameOutput = new JTextArea(32, 20);
 
     public GamePanel(GameController gameController, Game game) {
         GamePanel.gameController = gameController;
@@ -38,18 +36,17 @@ public class GamePanel extends JPanel {
         setLayout(new BorderLayout());
         setBounds(150, 150, 100, 50);
 
-        freeRoamTools.setLayout(new FlowLayout(FlowLayout.LEADING));
-//        enemyEncounterTools.setLayout(new FlowLayout(FlowLayout.LEADING));
-//        lootingTools.setLayout(new FlowLayout(FlowLayout.LEADING));
+        gamePanelControls.setLayout(new FlowLayout(FlowLayout.CENTER));
 
         north.addActionListener(new commandActionListener("north"));
         east.addActionListener(new commandActionListener("east"));
         west.addActionListener(new commandActionListener("west"));
         south.addActionListener(new commandActionListener("south"));
-        freeRoamTools.add(north);
-        freeRoamTools.add(east);
-        freeRoamTools.add(west);
-        freeRoamTools.add(south);
+        gamePanelControls.removeAll();
+        gamePanelControls.add(north);
+        gamePanelControls.add(west);
+        gamePanelControls.add(east);
+        gamePanelControls.add(south);
 
         // Save or exit game tools
         systemTools.setLayout(new BorderLayout());
@@ -59,59 +56,57 @@ public class GamePanel extends JPanel {
         systemTools.add(save, BorderLayout.NORTH);
         systemTools.add(exit, BorderLayout.SOUTH);
 
-        freeRoamTools.add(systemTools);
+        gamePanelControls.add(systemTools);
 
         gameOutput.setEditable(false);
 
         fight.addActionListener(new commandActionListener("fight"));
         run.addActionListener(new commandActionListener("run"));
-//        enemyEncounterTools.add(fight);
-//        enemyEncounterTools.add(run);
 
         equip.addActionListener(new commandActionListener("equip"));
         ignore.addActionListener(new commandActionListener("ignore"));
-//        lootingTools.add(equip);
-//        lootingTools.add(ignore);
 
         add(gameOutput, BorderLayout.NORTH);
-        add(freeRoamTools, BorderLayout.SOUTH);
+        add(gamePanelControls, BorderLayout.SOUTH);
     }
 
     private void setFreeRoam() {
-//        remove(enemyEncounterTools);
-//        remove(lootingTools);
-//        add(freeRoamTools, BorderLayout.SOUTH);
-        System.out.println("GamePanel: setFreeRoam reached");
-        freeRoamTools.remove(fight);
-        freeRoamTools.remove(run);
-        freeRoamTools.remove(equip);
-        freeRoamTools.remove(ignore);
-        freeRoamTools.revalidate();
-        freeRoamTools.repaint();
+        gamePanelControls.removeAll();
+        gamePanelControls.add(north);
+        gamePanelControls.add(west);
+        gamePanelControls.add(east);
+        gamePanelControls.add(south);
+        gamePanelControls.add(systemTools);
+        gamePanelControls.revalidate();
+        gamePanelControls.repaint();
     }
 
     private void setFightOrFlight() {
-//        remove(freeRoamTools);
-//        add(enemyEncounterTools);
-        System.out.println("GamePanel: setFightOrFlight reached");
-//        freeRoamTools.
-        freeRoamTools.add(fight);
-        freeRoamTools.add(run);
-        freeRoamTools.revalidate();
-        freeRoamTools.repaint();
+        gamePanelControls.add(fight);
+        gamePanelControls.add(run);
+        gamePanelControls.revalidate();
+        gamePanelControls.repaint();
     }
 
     private void setLootingMode() {
-//        remove(freeRoamTools);
-//        add(lootingTools);
-        freeRoamTools.add(equip);
-        freeRoamTools.add(ignore);
-        freeRoamTools.revalidate();
-        freeRoamTools.repaint();
+        gamePanelControls.remove(north);
+        gamePanelControls.remove(east);
+        gamePanelControls.remove(west);
+        gamePanelControls.remove(south);
+        gamePanelControls.add(equip);
+        gamePanelControls.add(ignore);
+        gamePanelControls.revalidate();
+        gamePanelControls.repaint();
+    }
+
+    private void setDeathScreenControls() {
+        gamePanelControls.removeAll();
+        gamePanelControls.add(exit);
+        gamePanelControls.revalidate();
+        gamePanelControls.repaint();
     }
 
     public void displayGame(String gameText) {
-//        gameController.generateMap();
         gameOutput.setText(gameText);
         revalidate();
         repaint();
@@ -139,19 +134,17 @@ public class GamePanel extends JPanel {
 
     public void youDied(String deathMessage) {
         displayGame(deathMessage);
-
+        setDeathScreenControls();
     }
 
     private class commandActionListener implements ActionListener {
         String command;
-//        int positionState = 0;
 
         public commandActionListener(String command) {
             this.command = command;
         }
 
         public void actionPerformed(ActionEvent e) {
-//            positionState = gameController.moveHero(direction);
             if (command.equals("fight") || command.equals("run")) {
                 gameController.fightControls(command);
             } else if (command.equals("equip") || command.equals("ignore")) {
