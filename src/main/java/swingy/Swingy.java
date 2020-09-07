875f175a594e059e0a74d5262e46d4743d66c021
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-09-07 15:24:57 +0200

    invalidParameterException added

--- src/main/java/swingy/Swingy.java
+++ src/main/java/swingy/Swingy.java
@@ -1,14 +1,22 @@
 package swingy;
 
+import java.security.InvalidParameterException;
+
 import swingy.model.UserInterface;
 
 public class Swingy {
 
 	public static void main (String[] args) {
 		try {
+			if (!args[0].equalsIgnoreCase("console") && !args[0].equalsIgnoreCase("gui")) {
+				throw new InvalidParameterException();
+			}
 			UserInterface userInterface = new UserInterface(args[0]);
 		} catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("Error: No interface argument. GUI or Console?");
+			System.exit(1);
+		} catch (InvalidParameterException e) {
+			System.out.println("Error: I'd argue that that was a bad argument... GUI or Console?");
 			System.exit(1);
 		}
     }
